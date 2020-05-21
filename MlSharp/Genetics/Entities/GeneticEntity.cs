6a561d7030074b3008ef4f4f3f56ca1a55c42495
Khalil <khalil.bello@outsourceglobal.com>
2020-05-22 00:33:41 +0100

    Created functions for breeding with partner.

--- MlSharp/Genetics/Entities/GeneticEntity.cs
+++ MlSharp/Genetics/Entities/GeneticEntity.cs
@@ -7,28 +7,19 @@ using ml_sharp.Utils;
 namespace ml_sharp.Genetics.Entities
 {
     /// <summary>
-    /// Defines an MlSharp genetic algorithm entity. A genetic Node
+    ///     Defines an MlSharp genetic algorithm entity. A genetic Node
     /// </summary>
     public class GeneticEntity : MlSharpNode
     {
         /// <summary>
-        /// List of all traits belonging to current GeneticEntity
-        /// </summary>
-        public List<Trait> Traits { get; set; }
-
-        /// <summary>
-        /// Measure of genes that current node is able to pass on to offspring.
-        /// This determines how likely it is for current entity to pass all of it's genes to it's offspring.
-        /// When genes get passed on to offspring, Genetic strength of mating partner (if any) is also considered.
-        /// </summary>
-        public float GeneticStrength { get; set; }
-
-        /// <summary>
-        /// Creates a genetic entity (node).
+        ///     Creates a genetic entity (node).
         /// </summary>
         /// <param name="name">Name given to current node/entity</param>
         /// <param name="traits">Traits belonging to current node/entity</param>
-        /// <param name="geneticStrength">Genes that current node can pass onto next generation during breeding. Max is 1.0, min 0.0f</param>
+        /// <param name="geneticStrength">
+        ///     Genes that current node can pass onto next generation during breeding. Max is 1.0, min
+        ///     0.0f
+        /// </param>
         public GeneticEntity(string name, List<Trait> traits, float geneticStrength)
         {
             Name = name;
@@ -37,8 +28,8 @@ namespace ml_sharp.Genetics.Entities
         }
 
         /// <summary>
-        /// Creates a genetic entity (node)
-        /// When this constructor is used, GeneticStrength is set to a value of 1.0f
+        ///     Creates a genetic entity (node)
+        ///     When this constructor is used, GeneticStrength is set to a value of 1.0f
         /// </summary>
         /// <param name="name">Name of the entity (name of the entity)</param>
         /// <param name="traits">List of traits that make up the entity</param>
@@ -47,8 +38,8 @@ namespace ml_sharp.Genetics.Entities
         }
 
         /// <summary>
-        /// Creates a genetic entity (node).
-        /// When this constructor is used, entity name is set to an empty string.
+        ///     Creates a genetic entity (node).
+        ///     When this constructor is used, entity name is set to an empty string.
         /// </summary>
         /// <param name="traits">Traits belonging to current node/entity</param>
         /// <param name="genesToPassOn">Genes that current node can pass onto next generation during breeding. Max is 1.0, min 0.0f</param>
@@ -57,15 +48,28 @@ namespace ml_sharp.Genetics.Entities
         }
 
         /// <summary>
-        /// Creates a genetic entity (node).
-        /// When this constructor is used, entity name is set to an empty string, GeneticStrength 1.0f and Traits an empty list of traits.
+        ///     Creates a genetic entity (node).
+        ///     When this constructor is used, entity name is set to an empty string, GeneticStrength 1.0f and Traits an empty list
+        ///     of traits.
         /// </summary>
         public GeneticEntity() : this("", new List<Trait>(), 1.0f)
         {
         }
 
         /// <summary>
-        /// Adds a new trait to the list of traits belonging to current GeneticEntity/entity.
+        ///     List of all traits belonging to current GeneticEntity
+        /// </summary>
+        public List<Trait> Traits { get; set; }
+
+        /// <summary>
+        ///     Measure of genes that current node is able to pass on to offspring.
+        ///     This determines how likely it is for current entity to pass all of it's genes to it's offspring.
+        ///     When genes get passed on to offspring, Genetic strength of mating partner (if any) is also considered.
+        /// </summary>
+        public float GeneticStrength { get; set; }
+
+        /// <summary>
+        ///     Adds a new trait to the list of traits belonging to current GeneticEntity/entity.
         /// </summary>
         /// <param name="trait">Trait to add to the list.</param>
         public void AddTrait(Trait trait)
@@ -82,36 +86,79 @@ namespace ml_sharp.Genetics.Entities
         }
 
         /// <summary>
-        /// Reproduces an offspring using current entity as parent.
+        ///     Reproduces an offspring using current entity as parent.
         /// </summary>
-        /// <param name="mutationValue">Determines the value by which mutation affects traits of resulting entity (between 0 and 1).</param>
-        /// <param name="mutationEffect">Determines how mutation affects value traits inherited by offspring.
-        /// (Positively, negatively, or randomly meaning some traits positively and some negatively at random)</param>
+        /// <param name="mutationValue">
+        ///     Determines the value by which mutation affects traits of resulting entity (between 0 and
+        ///     1).
+        /// </param>
+        /// <param name="mutationEffect">
+        ///     Determines how mutation affects value traits inherited by offspring.
+        ///     (Positively, negatively, or randomly meaning some traits positively and some negatively at random)
+        /// </param>
         /// <param name="offspringName">Name of resulting offspring (empty string if not set).</param>
-        /// <param name="seed">Seed for random generator. This is useful when trying to recreate outputs.
-        /// When 0 is set, no seed is used. 0 is set by default</param>
+        /// <param name="seed">
+        ///     Seed for random generator. This is useful when trying to recreate outputs.
+        ///     When 0 is set, no seed is used. 0 is set by default
+        /// </param>
         /// <returns>Returns a new GeneticEntity (offspring) built with criteria of current entity as base.</returns>
         public GeneticEntity ReproduceOne(float mutationValue, EMutationEffect mutationEffect = EMutationEffect.Random,
             string offspringName = "", int seed = 0)
         {
             var info = new ReproductionInfo(1, mutationValue, mutationEffect,
-                false, null, new string[] {offspringName}, seed);
+                false, null, new[] {offspringName}, seed);
 
             return (GeneticEntity) GeneticAlgorithm.YieldOffspring(info);
         }
 
         /// <summary>
-        /// Reproduces several offspring using current entity as parent.
+        ///     Reproduces an offspring using current entity and partner as parents.
+        /// </summary>
+        /// <param name="partner">Partner to reproduce with. Another entity</param>
+        /// <param name="mutationValue">
+        ///     Determines the value by which mutation affects traits of resulting entity (between 0 and
+        ///     1).
+        /// </param>
+        /// <param name="mutationEffect">
+        ///     Determines how mutation affects value traits inherited by offspring.
+        ///     (Positively, negatively, or randomly meaning some traits positively and some negatively at random)
+        /// </param>
+        /// <param name="offspringName">Name of resulting offspring (empty string if not set).</param>
+        /// <param name="seed">
+        ///     Seed for random generator. This is useful when trying to recreate outputs.
+        ///     When 0 is set, no seed is used. 0 is set by default
+        /// </param>
+        /// <returns>Returns a new GeneticEntity (offspring) built with criteria of current entity as base.</returns>
+        public GeneticEntity ReproduceOneWithPartner(GeneticEntity partner, float mutationValue,
+            EMutationEffect mutationEffect = EMutationEffect.Random, string offspringName = "", int seed = 0)
+        {
+            var info = new ReproductionInfo(1, mutationValue, mutationEffect,
+                true, partner, new[] {offspringName}, seed);
+
+            return (GeneticEntity) GeneticAlgorithm.YieldOffspring(info);
+        }
+
+        /// <summary>
+        ///     Reproduces several offspring using current entity as parent.
         /// </summary>
         /// <param name="numOffspring">Number of offspring to reproduce.</param>
-        /// <param name="mutationValue">Determines the value by which mutation affects traits of resulting entity (between 0 and 1).</param>
-        /// <param name="mutationEffect">Determines how mutation affects value traits inherited by offspring.
-        /// (Positively, negatively, or randomly meaning some traits positively and some negatively at random)</param>
-        /// <param name="offspringNames">Array of names to be given to resulting offspring (empty strings if not set).
-        /// If length of names array is shorter than numOffspring, the first n offspring will be assigned names from this list
-        /// and the rest set to an empty string.</param>
-        /// <param name="seed">Seed for random generator. This is useful when trying to recreate outputs.
-        /// When 0 is set, no seed is used. 0 is set by default</param>
+        /// <param name="mutationValue">
+        ///     Determines the value by which mutation affects traits of resulting entity (between 0 and
+        ///     1).
+        /// </param>
+        /// <param name="mutationEffect">
+        ///     Determines how mutation affects value traits inherited by offspring.
+        ///     (Positively, negatively, or randomly meaning some traits positively and some negatively at random)
+        /// </param>
+        /// <param name="offspringNames">
+        ///     Array of names to be given to resulting offspring (empty strings if not set).
+        ///     If length of names array is shorter than numOffspring, the first n offspring will be assigned names from this list
+        ///     and the rest set to an empty string.
+        /// </param>
+        /// <param name="seed">
+        ///     Seed for random generator. This is useful when trying to recreate outputs.
+        ///     When 0 is set, no seed is used. 0 is set by default
+        /// </param>
         /// <returns>Returns a new GeneticEntity (offspring) built with criteria of current entity as base.</returns>
         public List<GeneticEntity> ReproduceBatch(int numOffspring, float mutationValue,
             EMutationEffect mutationEffect = EMutationEffect.Random, string[] offspringNames = null, int seed = 0)
@@ -123,7 +170,40 @@ namespace ml_sharp.Genetics.Entities
         }
 
         /// <summary>
-        /// Return current entity as a dictionary.
+        ///     Reproduces several offspring using current entity and partner as parents.
+        /// </summary>
+        /// <param name="partner">Partner to reproduce with. Another entity</param>
+        /// <param name="numOffspring">Number of offspring to reproduce.</param>
+        /// <param name="mutationValue">
+        ///     Determines the value by which mutation affects traits of resulting entity (between 0 and
+        ///     1).
+        /// </param>
+        /// <param name="mutationEffect">
+        ///     Determines how mutation affects value traits inherited by offspring.
+        ///     (Positively, negatively, or randomly meaning some traits positively and some negatively at random)
+        /// </param>
+        /// <param name="offspringNames">
+        ///     Array of names to be given to resulting offspring (empty strings if not set).
+        ///     If length of names array is shorter than numOffspring, the first n offspring will be assigned names from this list
+        ///     and the rest set to an empty string.
+        /// </param>
+        /// <param name="seed">
+        ///     Seed for random generator. This is useful when trying to recreate outputs.
+        ///     When 0 is set, no seed is used. 0 is set by default
+        /// </param>
+        /// <returns>Returns a new GeneticEntity (offspring) built with criteria of current entity as base.</returns>
+        public List<GeneticEntity> ReproduceBatchWithPartner(GeneticEntity partner, int numOffspring,
+            float mutationValue,
+            EMutationEffect mutationEffect = EMutationEffect.Random, string[] offspringNames = null, int seed = 0)
+        {
+            var info = new ReproductionInfo(numOffspring, mutationValue, mutationEffect,
+                true, partner, offspringNames, seed);
+
+            return (List<GeneticEntity>) GeneticAlgorithm.YieldOffspring(info);
+        }
+
+        /// <summary>
+        ///     Return current entity as a dictionary.
         /// </summary>
         /// <returns>Current entity as a dictionary.</returns>
         public override Dictionary<string, object> AsDictionary()
@@ -134,7 +214,7 @@ namespace ml_sharp.Genetics.Entities
         }
 
         /// <summary>
-        /// Get all traits of current entity
+        ///     Get all traits of current entity
         /// </summary>
         /// <returns>Returns all traits belonging to current entity</returns>
         private Dictionary<string, object> GetAllTraitsAsDictionary()
